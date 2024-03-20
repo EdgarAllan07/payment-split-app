ad2874831f4b72cfece4ecf3996775f33937432c
Allan <108568433+EdgarAllan07@users.noreply.github.com>
2024-03-20 17:41:48 -0600

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,13 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" type="image/png" sizes="32x32"  href="./favicon.ico"/>
+    <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "ljv694sx4n");
+</script>
       </head>
       <body>
         <Layout>
